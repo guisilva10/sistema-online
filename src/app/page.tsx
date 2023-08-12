d4be8373e9dfb21a8fe89dc2eaf62f53f59a9690
Guilherme Lopes <guisilva.070104@gmail.com>
2023-08-12 02:23:03 -0300

    add form login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '../styles/page.module.scss'
 
 export default function Home() {
   return (
-    <div>
-      Ola Mundo
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <form className={styles.form}>
+          <h1>Olá, Seja Bem Vindo!</h1>
+         <div className={styles.formGroup}>
+         <label htmlFor="name">Nome</label>
+          <input type="text" name="name" id="name" />
+         </div>
+         <div className={styles.formGroup}>
+         <label htmlFor="name">Email</label>
+          <input type="email" name="email" id="email" />
+         </div>
+         <button className={styles.btn}>
+          Enviar
+         </button>
+        </form>
+      </div>
     </div>
   )
 }
